a63e2cb58304ebf390dec9e0d0530bc00b1048ce
AravindGunde0 <157368373+AravindGunde0@users.noreply.github.com>
2024-08-12 16:46:01 +0000

    Comparision of datatypes

---
+++ 01_basics/03_comparisions.js
@@ -0,0 +1,21 @@
+
+// console.log(1>2);
+// console.log(3<=5);
+// console.log(4!=9);
+// console.log(1==2);
+
+// console.log("2">1);//-->true
+// console.log(3>"2");
+
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null<=0);//___true
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// Strict Check include datatype comparision
+
+console.log("2"===2);// FAlse
+
