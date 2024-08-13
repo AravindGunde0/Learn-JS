22ac4295959efa9fb5a3be98686929610b80ab4e
AravindGunde0 <157368373+AravindGunde0@users.noreply.github.com>
2024-08-13 16:22:10 +0000

    operators

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -49,3 +49,30 @@ console.log(typeof heros);//object
 //String--> String
 //objecct-->object
 
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Memory 
+
+//stack(primitive)
+//heap(non-primitive)
+
+
+//stack--When stack memory is used, the variable is of copy value
+//heap--When heap memory is used, the variable is of reference valuee where it changes original value
+
+let myName="Aravind";
+let nm=myName;
+nm="Vinny"; //Created a copy and changes value in copy
+console.log(myName);
+console.log(nm);
+
+
+let add={
+    name:"bunny",
+}
+let dd=add;
+dd.name="funny";//it is refered in heap memory and updated at the original value
+console.log(add.name);
+console.log(dd.name);
