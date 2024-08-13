5dfae7cb7bb6e5314c54b836557ae59e3d011004
AravindGunde0 <157368373+AravindGunde0@users.noreply.github.com>
2024-08-13 17:05:39 +0000

    nums and maths

---
+++ 01_basics/05_nums_and_maths.js
@@ -0,0 +1,10 @@
+const num=100;
+
+const nnum=new Number(100);
+console.log(nnum);
+
+console.log(nnum.toString().length);
+console.log(nnum.toFixed(3));//This gives precision values after decimal
+
+const onum=256.25;
+console.log(onum.toPrecision(4));
